33f193ec008f6a7809275c9b33063e509a4fe8ab
Felix Lindemann <felixlindemann@web.de>
2024-07-19 10:40:25 +0200

    f

---
+++ blinkyMCP23S17.py
@@ -0,0 +1,22 @@
+from RPiMCP23S17.MCP23S17 import MCP23S17
+import time
+
+mcp1 = MCP23S17(bus=0x00, pin_cs=0x01, device_id=0x00)
+
+mcp1.open()
+
+
+for x in range(0, 16):
+    mcp1.setDirection(x, mcp1.DIR_OUTPUT)
+    
+
+print ("Starting blinky on all pins (CTRL+C to quit)")
+while (True):
+    for x in range(0, 16):
+        mcp1.digitalWrite(x, MCP23S17.LEVEL_HIGH)        
+        time.sleep(0.1)
+
+    for x in range(0, 16):
+        mcp1.digitalWrite(x, MCP23S17.LEVEL_LOW)
+        time.sleep(0.1)
+ 
---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt update
+
+sudo apt install python3-pip
+
+
+pip3 install RPiMCP23S17                               
+o1b95a
